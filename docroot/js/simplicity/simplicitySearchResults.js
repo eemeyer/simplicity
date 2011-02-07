1d89844ce7a9f7aa0ced9a928cde9e2db9cce810
Terry Smith <terry@t11e.com>
2011-02-07 17:51:16 -0500

    Adds jsdoc for simplicitySearchResults.

--- docroot/js/simplicity/simplicitySearchResults.js
+++ docroot/js/simplicity/simplicitySearchResults.js
@@ -1,24 +1,43 @@
 /**
  * @name $.ui.simplicitySearchResults
- * @namespace Widget that displays the search results
+ * @namespace Widget that displays the search results.
  *
- * <h2>Options</h2>
- * <dl>
- *   <dt>searchElement</dt>
- *   <dd>
- *     The simplicityDiscoverySearch widget that this widget binds it's events to. Defaults to <code>'body'</code>.
- *   </dd>
- * </dl>
+ * This widget listens to the <code>simplicitySearchResponse</code> event and takes
+ * the <code>resultsHtml</code> field from the search response, injecting it directly
+ * into the page.
+ * <p>
+ * If your search controller does not inject the search results as HTML into it's response
+ * then consider using <code>simplicityRenderParamsSearchResults</code> instead.
  */
 (function ($) {
   $.widget("ui.simplicitySearchResults", {
+    /**
+     * Widget options.
+     *
+     * <dl>
+     *   <dt>searchElement</dt>
+     *   <dd>
+     *     The simplicityDiscoverySearch widget that this widget binds it's events to. Defaults to <code>'body'</code>.
+     *   </dd>
+     * </dl>
+     * @name $.ui.simplicitySearchResults.options
+     */
     options: {
-      stateElement: 'body'
+      searchElement: 'body'
     },
     _create : function () {
       this.element.addClass('ui-simplicity-search-results');
-      $(this.options.stateElement).bind('simplicitySearchResponse', $.proxy(this._searchResponseHandler, this));
+      $(this.options.searchElement).bind('simplicitySearchResponse', $.proxy(this._searchResponseHandler, this));
     },
+    /**
+     * Event handler for the <code>simplicitySearchResponse</code> event. Expects the given
+     * search response object to contain a <code>resultsHtml</code> field and splices it
+     * directly into the page.
+     *
+     * @name $.ui.simplicitySearchResults._searchResponseHandler
+     * @function
+     * @private
+     */
     _searchResponseHandler: function (evt, searchResponse) {
       var resultsHtml = searchResponse.resultsHtml || '';
       if (searchResponse.error) {
@@ -31,7 +50,7 @@
     },
     destroy: function () {
       this.element.removeClass('ui-simplicity-search-results');
-      $(this.options.stateElement).unbind('simplicitySearchResponse', this._searchResponseHandler);
+      $(this.options.searchElement).unbind('simplicitySearchResponse', this._searchResponseHandler);
       $.Widget.prototype.destroy.apply(this, arguments);
     }
   });
