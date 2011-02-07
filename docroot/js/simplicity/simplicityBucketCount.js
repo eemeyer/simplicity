f57773d8722772023f768faefe0144ced9d4d20b
Terry Smith <terry@t11e.com>
2011-02-07 16:23:31 -0500

    Adds jsdoc for simplicityBucketCount.

--- docroot/js/simplicity/simplicityBucketCount.js
+++ docroot/js/simplicity/simplicityBucketCount.js
@@ -1,9 +1,72 @@
 /**
  * @name $.ui.simplicityBucketCount
- * @namespace Displays a single bucket count based on the search results
+ * @namespace Displays a single bucket count based on the search results.
+ *
+ * Listens for simplicityBucketCounts events and updates element HTML with a templatized
+ * bucket count. Also supports injecting the bucket count into a select input.
+ * <p>
+ * This widget is generally used as a building block, you probably want to use
+ * <code>simplicityFacetedInput</code> directly.
+ *
+ * @example
+ *   &lt;select id="size" name="size">
+ *     &lt;option value="small">Small&lt;/option>
+ *     &lt;option value="medium">Medium&lt;/option>
+ *     &lt;option value="large">Large&lt;/option>
+ *   &lt;/select>
+ *   &lt;script type="text/javascript>
+ *     $('#size option[value="small"]').simplicityBucketCount({
+ *       dimension: 'Size',
+ *       bucketId: 'S'
+ *     });
+ *     // etc.
+ *   &lt;/script>
+ *
+ * @example
+ *   &lt;input type="checkbox" id="large" name="size" value="large" />
+ *   &lt;label for="large">Large (&lt;span id="largeCount">&lt;/span>)&lt;/label>
+ *   &lt;script type="text/javascript>
+ *     $('#largeCount').simplicityBucketCount({
+ *       dimension: 'Size',
+ *       bucketId: 'L'
+ *     });
+ *   &lt;/script>
  */
 (function ($) {
   $.widget("ui.simplicityBucketCount", {
+    /**
+     * Widget options.
+     *
+     * <dl>
+     *   <dt>searchElement</dt>
+     *   <dd>
+     *     The location of the simplicityDiscoverySearch widget. Defaults to <code>'body'</code>.
+     *   </dd>
+     *   <dt>missingText</dt>
+     *   <dd>
+     *     Text to use when there is no associated bucket count. Defaults to <code>'?'</code>.
+     *   </dd>
+     *   <dt>dimension</dt>
+     *   <dd>
+     *     Mandatory dimension from which the bucket counts should be used.
+     *   </dd>
+     *   <dt>bucketId</dt>
+     *   <dd>
+     *     Mandatory id of the bucket whose count should be bound to this widget.
+     *   </dd>
+     *   <dt>optionTemplate</dt>
+     *   <dd>
+     *     Template used when bound to an <code>OPTION</code> element.
+     *     Defaults to <code>'{option} {count}'</code>.
+     *   </dd>
+     *   <dt>numberFormatter</dt>
+     *   <dd>
+     *     Optional function that can be called with the bucket count and is expected
+     *     to return a string.
+     *   </dd>
+     * </dl>
+     * @name $.ui.simplicityBucketCount.options
+     */
     options : {
       searchElement: 'body',
       missingText: '?',
@@ -28,6 +91,14 @@
       }
       $(this.options.searchElement).bind('simplicityBucketCounts', $.proxy(this._bucketCountsHandler, this));
     },
+    /**
+     * Event handler for the <code>simplicityBucketCounts<code> event.
+     * Extracts the configured bucket count and displays it.
+     *
+     * @name $.ui.simplicityBucketCount._bucketCountsHandler
+     * @function
+     * @private
+     */
     _bucketCountsHandler: function (event, dimFacetCounts) {
       var result = undefined;
       var facetCounts = dimFacetCounts[this.options.dimension];
