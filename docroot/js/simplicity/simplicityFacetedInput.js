7fb4c2baac7340a0c6654cf177a4e0bfd4609467
Terry Smith <terry@t11e.com>
2011-02-07 17:01:30 -0500

    Adds jsdoc for simplicityFacetedInput.

--- docroot/js/simplicity/simplicityFacetedInput.js
+++ docroot/js/simplicity/simplicityFacetedInput.js
@@ -1,9 +1,94 @@
 /**
  * @name $.ui.simplicityFacetedInput
- * @namespace Combines simplicityInputs and simplicityBucketCount
+ * @namespace Combines simplicityInputs and simplicityBucketCount.
+ * <p>
+ * Convenience wrapper around <code>simplicityInputs</code> and <code>simplicityBucketCount</code>.
+ * <p>
+ * The options for this widget are passed through to both of the contained widgets.
+ * Additionally, the option <code>bucketId</code> is automatically created for the
+ * <code>simplicityBucketCount</code> widget from attributes on the wrapped input.
  */
 (function ($) {
   $.widget("ui.simplicityFacetedInput", {
+    /**
+     * Widget options.
+     *
+     * <dl>
+     *   <dt>dimension</dt>
+     *   <dd>
+     *     Mandatory option, used to determine which dimension is used for bucket counts.
+     *     Defaults to <code>''</code>.
+     *   </dd>
+     *   <dt>template</dt>
+     *   <dd>
+     *     For when this widget is bound to an <code>input</code> other than <code>select</code>.
+     *     Is used as the element for the <code>simplicityBucketCounts</code> widget.
+     *     Defaults to <code>&lt;span/></code>.
+     *   </dd>
+     *   <dt>placement</dt>
+     *   <dd>
+     *     For when this widget is bound to an <code>input</code> other than <code>select</code>.
+     *     Determines where to place the automatically created <code>simplicityBucketCounts</code>
+     *     widget. Defaults to <code>after-label</code>. Possible options are
+     *     <dl>
+     *       <dt>before-input</dt>
+     *       <dd>
+     *         Immediately before the <code>input</code> element.
+     *         <pre>
+     *            HERE
+     *            &lt;input type="checkbox" id="large" name="size" value="large">
+     *            &lt;label for="large">Large&lt;/label>
+     *         </pre>
+     *       </dd>
+     *       <dt>after-input</dt>
+     *       <dd>
+     *         Immediately after the <code>input</code> element.
+     *         <pre>
+     *            &lt;label for="large">Large&lt;/label>
+     *            &lt;input type="checkbox" id="large" name="size" value="large">
+     *            HERE
+     *         </pre>
+     *       </dd>
+     *       <dt>before-label</dt>
+     *       <dd>
+     *         Immediately before the <code>label</code> associated with the <code>input</code> element.
+     *         <pre>
+     *            HERE
+     *            &lt;label for="large">Large&lt;/label>
+     *            &lt;input type="checkbox" id="large" name="size" value="large">
+     *         </pre>
+     *       </dd>
+     *       <dt>after-label</dt>
+     *       <dd>
+     *         Immediately after the <code>label</code> associated with the <code>input</code> element.
+     *         <pre>
+     *            &lt;input type="checkbox" id="large" name="size" value="large">
+     *            &lt;label for="large">Large&lt;/label>
+     *            HERE
+     *         </pre>
+     *       </dd>
+     *       <dt>append-label</dt>
+     *       <dd>
+     *         Appended into the <code>label</code> associated with the <code>input</code> element.
+     *         <pre>
+     *            &lt;input type="checkbox" id="large" name="size" value="large">
+     *            &lt;label for="large">Large HERE&lt;/label>
+     *         </pre>
+     *       </dd>
+     *       <dd></dd>
+     *       <dt>prepend-label</dt>
+     *       <dd>
+     *         Prepended into the <code>label</code> associated with the <code>input</code> element.
+     *         <pre>
+     *            &lt;input type="checkbox" id="large" name="size" value="large">
+     *            &lt;label for="large">HERE Large&lt;/label>
+     *         </pre>
+     *       </dd>
+     *     </dl>
+     *   </dd>
+     * </dl>
+     * @name $.ui.simplicityFacetedInput.options
+     */
     options : {
       dimension: '',
       template: '<span/>',
