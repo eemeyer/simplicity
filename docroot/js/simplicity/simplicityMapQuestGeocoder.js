35351f5a25f0cc04f7233800960f34b284147e34
Terry Smith <terry@t11e.com>
2011-02-08 14:17:08 -0500

    Adds jsdoc for simlicityMapQuestGeocoder.

--- docroot/js/simplicity/simplicityMapQuestGeocoder.js
+++ docroot/js/simplicity/simplicityMapQuestGeocoder.js
@@ -1,19 +1,88 @@
 /**
  * @name $.ui.simplicityMapQuestGeocoder
- * @namespace A MapQuest geocoder
+ * @namespace A MapQuest geocoder.
+ * <p>
+ * Google geocoder widget that wraps the provides standardized
+ * callbacks across vendors and normalized results. Also provides
+ * a jQueri UI autocomplete source.
+ * <p>
+ * See the MapQuest JavaScript SDK v6 Geocoding <a href="http://platform.beta.mapquest.com/sdk/js/v6.0.0/geocoding.html">documentation</a>.
  */
 (function ($) {
   $.widget("ui.simplicityMapQuestGeocoder", {
+    /**
+     * Widget options.
+     *
+     * <dl>
+     *   <dt>geocodeOptions</dt>
+     *   <dd>
+     *     Options passed to the MapQuest geocode function.
+     *     Defaults to <code>{thumbMaps: false}</code>.
+     *   </dd>
+     *   <dt>disambiguateResults</dt>
+     *   <dd>
+     *     When <code>true</code> post-processes the normalized items to look for
+     *     any duplicates and alter their <code>value</code>s to allow for
+     *     disambiguation. Defaults to <code>true</code>.
+     *   </dd>
+     *   <dt>debug</dt>
+     *   <dd>
+     *     Enable logging of key events to console.log. Defaults to <code>false</code>.
+     *   </dd>
+     * </dl>
+     * @name $.ui.simplicityMapQuestGeocoder.options
+     */
     options : {
-      disambiguateResults: true,
-      debug: false,
       geocodeOptions: {
         thumbMaps: false
-      }
+      },
+      disambiguateResults: true,
+      debug: false
     },
     _create: function () {
       this.element.addClass('ui-simplicity-mapquest-geocoder');
     },
+    /**
+     * Perform geocoding of the <code>geocodeRequest</code> calling the
+     * <code>callback</code> on completion.
+     * <p>
+     * Triggers two events.
+     * <dl>
+     *   <dt><code>request</code></dt>
+     *   <dd>
+     *     Final step before sending the geocode request, it is passed
+     *     as the custom object to this event to allow for manipulation.
+     *   </dd>
+     *   <dt><code>response</code></dt>
+     *   <dd>
+     *     Final step before calling the <code>callback</code> with the
+     *     geocode response to allow for manipulaton.
+     *     Triggered after any normalization phase.
+     *     <p>
+     *     The custom object passed to this event looks like so:
+     *     <pre>
+     *     {
+     *       response: {
+     *         // Original vendor response
+     *       },
+     *       items: [
+     *         // Array of normalized items, see <code>normalizeResults</code>.
+     *       ]
+     *     }
+     *     </pre>
+     *   </dd>
+     * </dl>
+     *
+     * @param geocodeRequest
+     *   The request as expected by the upstream geocode vendor.
+     * @param callback
+     *   The callback triggered at geocode completion. Is give one
+     *   argument which is the same as custom <code>Object</code>
+     *   passed to the <code>response</code> event.
+     *
+     * @name $.ui.simplicityMapQuestGeocoder.geocode
+     * @function
+     */
     geocode: function (locations, callback) {
       var geocodeRequest = {
         locations: locations,
@@ -36,6 +105,13 @@
         callback(response);
       }, this));
     },
+    /**
+     * Create an autocomplete source function that returns normalized
+     * geocoded addresses.
+     *
+     * @name $.ui.simplicityMapQuestGeocoder.autocompleteSource
+     * @function
+     */
     autocompleteSource: function () {
       return $.proxy(function (request, responseCallback) {
         this.geocode(request.term, function (response) {
@@ -43,6 +119,26 @@
         });
       }, this);
     },
+    /**
+     * Normalize the geocoder response. Returns an array of items.
+     * <pre>
+     *   Example output
+     *   [
+     *     {
+     *       value: 'Statue of Liberty, New York, NY 11231, USA',
+     *       latitude: 40.6892437,
+     *       longitude: -74.0445142,
+     *       response: {
+     *         // Upstream vendor response for this single location
+     *       }
+     *     }
+     *   ]
+     * </pre>
+     *
+     * @name $.ui.simplicityMapQuestGeocoder.normalizeResults
+     * @function
+     * @private
+     */
     normalizeResults: function (response) {
       var items = [];
       if (response.info.statuscode === 0) {
@@ -65,6 +161,14 @@
       }
       return items;
     },
+    /**
+     * Normalize a single geocoded address. This gets injected
+     * as the <code>value</code> of the <code>item</code> object.
+     *
+     * @name $.ui.simplicityMapQuestGeocoder.normalizeAddress
+     * @function
+     * @private
+     */
     normalizeAddress: function (location) {
       var value = location.street;
       if (location.adminArea5 !== '') {
