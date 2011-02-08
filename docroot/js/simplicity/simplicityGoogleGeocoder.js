dce3d25ad181ff030808b4b86353a945716687eb
Terry Smith <terry@t11e.com>
2011-02-08 13:18:43 -0500

    Adds jsdoc for simplicityGoogleGeocoder.

--- docroot/js/simplicity/simplicityGoogleGeocoder.js
+++ docroot/js/simplicity/simplicityGoogleGeocoder.js
@@ -1,9 +1,26 @@
 /**
  * @name $.ui.simplicityGoogleGeocoder
- * @namespace A Google geocoder
+ * @namespace A Google geocoder.
+ * <p>
+ * Google geocoder widget that wraps the provides standardized
+ * callbacks across vendors and normalized results. Also provides
+ * a jQueri UI autocomplete source.
+ * <p>
+ * See the Google Maps JavaScript API v3 <a href="http://code.google.com/apis/maps/documentation/javascript/services.html#Geocoding">documentation</a>.
  */
 (function ($) {
   $.widget("ui.simplicityGoogleGeocoder", {
+    /**
+     * Widget options.
+     *
+     * <dl>
+     *   <dt>debug</dt>
+     *   <dd>
+     *     Enable logging of key events to console.log. Defaults to <code>false</code>.
+     *   </dd>
+     * </dl>
+     * @name $.ui.simplicityGoogleGeocoder.options
+     */
     options : {
       debug: false
     },
@@ -11,6 +28,47 @@
       this.element.addClass('ui-simplicity-google-geocoder');
       this._geocoder = new google.maps.Geocoder();
     },
+    /**
+     * Perform geocoding of the <code>geocodeRequest</code> calling the
+     * <code>callback</code> on completion.
+     * <p>
+     * Triggers two events.
+     * <dl>
+     *   <dt><code>request</code></dt>
+     *   <dd>
+     *     Final step before sending the geocode request, it is passed
+     *     as the custom object to this event to allow for manipulation.
+     *   </dd>
+     *   <dt><code>response</code></dt>
+     *   <dd>
+     *     Final step before calling the <code>callback</code> with the
+     *     geocode response to allow for manipulaton.
+     *     Triggered after any normalization phase.
+     *     <p>
+     *     The custom object passed to this event looks like so:
+     *     <pre>
+     *     {
+     *       response: {
+     *         // Original vendor response
+     *       },
+     *       items: [
+     *         // Array of normalized items, see <code>normalizeResults</code>.
+     *       ]
+     *     }
+     *     </pre>
+     *   </dd>
+     * </dl>
+     *
+     * @param geocodeRequest
+     *   The request as expected by the upstream geocode vendor.
+     * @param callback
+     *   The callback triggered at geocode completion. Is give one
+     *   argument which is the same as custom <code>Object</code>
+     *   passed to the <code>response</code> event.
+     *
+     * @name $.ui.simplicityGoogleGeocoder.geocode
+     * @function
+     */
     geocode: function (geocodeRequest, callback) {
       this._trigger('request', {}, geocodeRequest);
       if (this.options.debug) {
@@ -19,7 +77,7 @@
       this._geocoder.geocode(geocodeRequest, $.proxy(function (results, status) {
         var items = this.normalizeResults(results, status);
         var response = {
-          results: results,
+          response: results,
           status: status,
           items: items
         };
@@ -30,6 +88,13 @@
         callback(response);
       }, this));
     },
+    /**
+     * Create an autocomplete source function that returns normalized
+     * geocoded addresses.
+     *
+     * @name $.ui.simplicityGoogleGeocoder.autocompleteSource
+     * @function
+     */
     autocompleteSource: function () {
       return $.proxy(function (request, responseCallback) {
         this.geocode({address: request.term}, function (response) {
@@ -37,6 +102,26 @@
         });
       }, this);
     },
+    /**
+     * Normalize the geocoder response. Returns an array of items.
+     * <pre>
+     *   Example output
+     *   [
+     *     {
+     *       value: 'Statue of Liberty, New York, NY 11231, USA',
+     *       latitude: 40.6892437,
+     *       longitude: -74.0445142,
+     *       response: {
+     *         // Upstream vendor response for this single location
+     *       }
+     *     }
+     *   ]
+     * </pre>
+     *
+     * @name $.ui.simplicityGoogleGeocoder.normalizeResults
+     * @function
+     * @private
+     */
     normalizeResults: function (results, status) {
       var items = [];
       if (status === google.maps.GeocoderStatus.OK) {
@@ -54,6 +139,14 @@
       }
       return items;
     },
+    /**
+     * Normalize a single geocoded address. This gets injected
+     * as the <code>value</code> of the <code>item</code> object.
+     *
+     * @name $.ui.simplicityGoogleGeocoder.normalizeAddress
+     * @function
+     * @private
+     */
     normalizeAddress: function (result) {
       return result.formatted_address;
     },
