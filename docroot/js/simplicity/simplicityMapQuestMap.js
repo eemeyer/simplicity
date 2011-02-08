c3eae86e1f09b92bf3731048bad76f80b182ac6f
Terry Smith <terry@t11e.com>
2011-02-08 12:51:44 -0500

    Adds jsdoc for simplicityMapQuestMap.

--- docroot/js/simplicity/simplicityMapQuestMap.js
+++ docroot/js/simplicity/simplicityMapQuestMap.js
@@ -1,21 +1,77 @@
 /**
  * @name $.ui.simplicityMapQuestMap
  * @namespace A MapQuest map
+ * <p>
+ * MapQuest Map widget that creates the map and listens for <code>simplicityResultSet</code> events
+ * which it uses to add markers to the map for the search results.
+ *
+ * @example
+ *   &lt;div id="map" style="width: 300px; height: 300px;">&lt;/div>
+ *   &lt;script type="text/javascript">
+ *     $('#map').simplicityMapQuestMap();
+ *   &lt;/script>
+ *
+ * @see MapQuest JavaScript SDK v6 <a href="http://platform.beta.mapquest.com/sdk/js/v6.0.0/">documentation</a>.
  */
 (function ($) {
   $.widget("ui.simplicityMapQuestMap", {
+    /**
+     * Widget options.
+     *
+     * <dl>
+     *   <dt>searchElement</dt>
+     *   <dd>
+     *     The simplicityDiscoverySearch widget that this widget binds it's events to. Defaults to <code>'body'</code>.
+     *   </dd>
+     *   <dt>latitudeField</dt>
+     *   <dd>
+     *     Field to find the latitude of the result item in the <code>simplicityResultSet</code>
+     *     item properties. Defaults to <code>'latitude'</code>.
+     *   </dd>
+     *   <dt>longitudeField</dt>
+     *   <dd>
+     *     Field to find the longitude of the result item in the <code>simplicityResultSet</code>
+     *     item properties. Defaults to <code>'longitude'</code>.
+     *   </dd>
+     *   <dt>mapOptions</dt>
+     *   <dd>
+     *     Options used when creating the map. Defaults to <code>''</code> which is expanded at
+     *     runtime to
+     *     <pre>
+     *     {
+     *       center: {lat: 0, lng: 0},
+     *       zoom: 1,
+     *       mapType: 'map'
+     *     }
+     *     </pre>
+     *     Can be either an <code>Object</code> or a <code>function</code>.
+     *   </dd>
+     * </dl>
+     * @name $.ui.simplicityMapQuestMap.options
+     */
     options : {
       searchElement: 'body',
       latitudeField: 'latitude',
       longitudeField: 'longitude',
+      mapOptions: '',
+      // The following options are for internal use only
       apiKey: '',
-      mapOptions: ''
+      mapVersion: '6.0.0'
     },
     _create: function () {
       this.element.addClass('ui-simplicity-mapquest-map');
       this._initWhenAvailable();
       $(this.options.searchElement).bind('simplicityResultSet', $.proxy(this._resultSetHandler, this));
     },
+    /**
+     * Lazy initialization method used to create the map only when the necessary JavaScript
+     * is available. Intended to be called from any of this widgets public methods that need to
+     * access the this._map.
+     *
+     * @name $.ui.simplicityMapQuestMap._initWhenAvailable
+     * @function
+     * @private
+     */
     _initWhenAvailable: function () {
       var available = false;
       if ('undefined' !== typeof this._map) {
@@ -24,12 +80,14 @@
         var mapOptions;
         if (this.options.mapOptions === '') {
           mapOptions = {
-            zoom: 1,
             center: {lat: 0, lng: 0},
+            zoom: 1,
             mapType: 'map'
           };
-        } else {
+        } else if ($.isFunction(this.options.mapOptions)) {
           mapOptions = this.options.mapOptions.call(this);
+        } else {
+          mapOptions = this.options.mapOptions;
         }
         this._map = new MQA.TileMap(this.element[0],
           this.options.zoom, this.options.center, this.options.mapType);
@@ -37,17 +95,32 @@
       }
       return available;
     },
+    /**
+     * Return the actual map object.
+     *
+     * @name $.ui.simplicityMapQuestMap.map
+     * @function
+     */
     map: function () {
       return this._map;
     },
+    /**
+     * Try to dynamically load the necessary JavaScript from the upstream vendor that will
+     * allow the map to function.
+     *
+     * @name $.ui.simplicityMapQuestMap.loadMap
+     * @function
+     * @private
+     */
     loadMap: function () {
       if ('undefined' === typeof this._map) {
         if ('undefined' === typeof MQA || 'undefined' === typeof MQA.TileMap) {
+          var src = 'http://mapquestapi.com/sdk/js/v' + this.options.mapVersion + '/mqa.toolkit.js';
           if ('undefined' === typeof $.simplicityLoadJs) {
+            src = src + '?key=YOUR_API_KEY';
             alert('Dynamic loading of MapQuest maps is not supported. Enable this widget by adding the following script tag to your page:\n\n' +
               '<script src="http://mapquestapi.com/sdk/js/v6.0.0/mqa.toolkit.js?key=YOUR_KEY"></script>');
           } else {
-            var src = 'http://mapquestapi.com/sdk/js/v6.0.0/mqa.toolkit.js';
             if (this.options.apiKey !== '') {
               src = src + '?key=' + this.options.apiKey;
             }
@@ -58,9 +131,27 @@
         }
       }
     },
+    /**
+     * Makes the widget re-handle the last <code>simplicityResultSet</code> event to reapply
+     * any map markers.
+     *
+     * @name $.ui.simplicityMapQuestMap.refreshMap
+     * @function
+     */
     refreshMap: function () {
       this._resultSetHandler({}, $(this.options.searchElement).simplicityDiscoverySearch('resultSet'));
     },
+    /**
+     * Event handler for the <code>simplicityResultSet</code> event. Extracts the coordinates
+     * of each result item by using the property fields defined by the
+     * <code>latitudeField</code> and <code>longitudeField</code> options of this widget and
+     * places a marker on the map for each valid coordinate. The map is then reset to best
+     * display the current set of markers.
+     *
+     * @name $.ui.simplicityMapQuestMap._resultSetHandler
+     * @function
+     * @private
+     */
     _resultSetHandler: function (evt, resultSet) {
       if (this._initWhenAvailable()) {
         this._map.removeAllShapes();
@@ -97,7 +188,15 @@
         }
       }
     },
-    // Ported from: http://www.widgetsandburritos.com/technical/programming/mapquest-api/
+    /**
+     * Calculates the best zoom level for a bounding box.
+     * <p>
+     * Ported from <a href="http://www.widgetsandburritos.com/technical/programming/mapquest-api/">this blog post</a>.
+     *
+     * @name $.ui.simplicityMapQuestMap._calculateZoomLevel
+     * @function
+     * @private
+     */
     _calculateZoomLevel: function (minLat, minLng, maxLat, maxLng) {
       var mapWidth = this.element.width();
       var mapHeight = this.element.height();
