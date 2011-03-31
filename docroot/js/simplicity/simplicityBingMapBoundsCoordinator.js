b3865671fd7a6e17f0e98e573f15f94f98aa7f26
Eric Meyer <eric@t11e.com>
2011-03-31 17:07:55 -0400

    Adds WIP breaking out simplicityBingMap to smaller components (following same pattern used for simplicityGoogleMap).

---
+++ docroot/js/simplicity/simplicityBingMapBoundsCoordinator.js
@@ -0,0 +1,80 @@
+/**
+ * @name $.ui.simplicityBingMapBoundsCoordinator
+ * @namespace A Bing map.
+ * <p>
+ * Bing Map widget that creates the map and listens for <code>simplicityResultSet</code> events
+ * which it uses to add markers to the map for the search results.
+ *
+ * @example
+ *   &lt;div id="map" style="position: absolute; width: 300px; height: 300px;">&lt;/div>
+ *   &lt;script type="text/javascript">
+ *     $('#map').simplicityBingMapBoundsCoordinator({
+ *       credentials: 'Your credentials go here'
+ *     });
+ *   &lt;/script>
+ *
+ * @see Bing Maps AJAX Control v7 <a href="http://msdn.microsoft.com/en-us/library/gg427610.aspx">documentation</a>.
+ */
+(function ($) {
+  $.widget("ui.simplicityBingMapBoundsCoordinator", {
+    /**
+     * Widget options.
+     *
+     * <dl>
+     *   <dt>map</dt>
+     *   <dd>
+     *     Optional map instance, if not provided one will be looked up. Defaults to <code>''</code>.
+     *   </dd>
+     *   <dt>searchElement</dt>
+     *   <dd>
+     *     The simplicityDiscoverySearch widget that this widget binds it's events to. Defaults to <code>'body'</code>.
+     *   </dd>
+     * </dl>
+     * @name $.ui.simplicityGoogleMapBoundsCoordinator.options
+     */
+    options: {
+      searchElement: 'body',
+      map: ''
+    },
+    _create : function () {
+      this.element.addClass('ui-simplicity-bing-map-bounds-coordinator');
+      this._map = this.options.map !== '' ? this.options.map : this.element.simplicityBingMap('map'); // TODO: will this work if map API is not yet loaded?
+      $(this.options.searchElement).bind('simplicitySearchResponseHandled', $.proxy(this._handler, this));
+    },
+    /**
+     * Return the actual map object.
+     *
+     * @name $.ui.simplicityBingMapBoundsCoordinator.map
+     * @function
+     */
+    map: function () {
+      return this._map;
+    },
+    _handler: function (evt) {
+      this.updateBounds();
+    },
+    /**
+     * Triggers a simplicitybingmapboundscoordinatorcalculatebounds event. Handlers for that event receive a ui
+     * object with a locations member. They can update, replace or delete that variable. ui.locations is defined and non-empty
+     * after the event is handled, then this component will update the bing map to fit the locations.
+     *
+     * @name $.ui.simplicityGoogleMapBoundsCoordinator.updateBounds
+     * @function
+     */
+    updateBounds: function () {
+      var ui = {
+        locations: []
+      };
+      this._trigger('calculateBounds', {}, ui);
+      if ('undefined' !== typeof ui.locations && 0 !== ui.locations.length) {
+        this._map.setView({bounds: Microsoft.Maps.LocationRect.fromLocations(ui.locations)});
+      }
+    },
+    destroy: function () {
+      this.element.removeClass('ui-simplicity-bing-map-bounds-coordinator');
+      $(this.options.searchElement).unbind('simplicitySearchResponseHandled', this._handler);
+      delete this._map;
+      $.Widget.prototype.destroy.apply(this, arguments);
+    }
+  });
+}(jQuery));
---
+++ docroot/js/simplicity/simplicityBingMapCriteria.js
@@ -0,0 +1,162 @@
+/**
+ * @name $.ui.simplicityBingMapCriteria
+ * @namespace A Bing map.
+ * <p>
+ * Bing Map widget that creates the map and listens for <code>simplicityResultSet</code> events
+ * which it uses to add markers to the map for the search results.
+ *
+ * @example
+ *   &lt;div id="map" style="position: absolute; width: 300px; height: 300px;">&lt;/div>
+ *   &lt;script type="text/javascript">
+ *     $('#map').simplicityBingMapCriteria({
+ *       credentials: 'Your credentials go here'
+ *     });
+ *   &lt;/script>
+ *
+ * @see Bing Maps AJAX Control v7 <a href="http://msdn.microsoft.com/en-us/library/gg427610.aspx">documentation</a>.
+ */
+(function ($) {
+  $.widget("ui.simplicityBingMapCriteria", {
+    /**
+     * Widget options.
+     *
+     * <dl>
+     *   <dt>searchElement</dt>
+     *   <dd>
+     *     The simplicityDiscoverySearch widget that this widget binds it's events to. Defaults to <code>'body'</code>.
+     *   </dd>
+     *   <dt>map</dt>
+     *   <dd>
+     *     Optional map instance, if not provided one will be looked up. Defaults to <code>''</code>.
+     *   </dd>
+     *   <dt>updateBounds</dt>
+     *   <dd>
+     *     Whether or not the map bounds should be updated to include the criteria locations. Defaults to true.
+     *   </dd>
+     * </dl>
+     * @name $.ui.simplicityBingMapCriteria.options
+     */
+    options: {
+      searchElement: 'body',
+      updateBounds: true,
+      map: ''
+    },
+    _create : function () {
+      this.element.addClass('ui-simplicity-bing-map-criteria');
+      this._map = this.options.map !== '' ? this.options.map : this.element.simplicityBingMap('map');
+      this._markers = [];
+      $(this.options.searchElement).bind('simplicitySearchResponse', $.proxy(this._responseHandler, this));
+      this.element.bind('simplicitybingmapboundscoordinatorcalculatebounds', $.proxy(this._calcBoundsHandler, this));
+    },
+    /**
+     * Return the actual map object.
+     *
+     * @name $.ui.simplicityBingMapCriteria.map
+     * @function
+     */
+    map: function () {
+      return this._map;
+    },
+    /**
+     * Makes the widget re-handle the last <code>simplicityResultSet</code> event to reapply
+     * any map markers.
+     *
+     * @name $.ui.simplicityBingMapCriteria.refreshMap
+     * @function
+     */
+    refreshMap: function () {
+      this.removeCriteria();
+      this.addCriteria();
+    },
+    /**
+     * Event handler for the <code>simplicitySearchResponse</code> event.
+     * Extracts the placemarks for all of the criteria locations, and places markers or shapes on the map for each.
+     *
+     * @name $.ui.simplicityBingMapCriteria._responseHandler
+     * @function
+     * @private
+     */
+    _responseHandler: function (evt, ui) {
+      this.removeCriteria();
+      this.addCriteria(ui);
+    },
+    _calcBoundsHandler: function (evt, ui) {
+      var locations = ui.locations;
+      if ('undefined' !== typeof locations && this.options.updateBounds) {
+        $.each(this._markers, function (pmIdx, poly) {
+          if (poly.getBounds) {
+            bounds = poly.getBounds();
+          } else if (poly.getPaths) {
+            $.each(poly.getPaths().getArray(), function (idx, path) {
+              $.each(path.getArray(), function (idx2, latlng) {
+                bounds.extend(latlng);
+              });
+            });
+          } else if (poly.getPath) {
+            $.each(poly.getPath().getArray(), function (idx, latlng) {
+              bounds.extend(latlng);
+            });
+          } else if (poly.getPosition) {
+            bounds.extend(poly.getPosition());
+          }
+        });
+      }
+    },
+    /**
+     * Removes any markers or shapes that were added to the map by <code>addCriteria</code>.
+     *
+     * @name $.ui.simplicityBingMapCriteria.removeCriteria
+     * @function
+     * @private
+     */
+    removeCriteria: function () {
+      $.each(this._markers, $.proxy(function (idx, marker) {
+        this._map.entities.remove(marker);
+      }, this));
+      this._markers.length = 0;
+    },
+    /**
+     * Adds any markers and shapes that can be extracted from the given <code>searchResponse</code>.
+     *
+     * @name $.ui.simplicityBingMapCriteria.addCriteria
+     * @function
+     * @private
+     */
+    addCriteria: function (ui) {
+      if ('undefined' === typeof ui) {
+        ui = $(this.options.searchElement).simplicityDiscoverySearch('searchResponse');
+      }
+      if ($.isArray(ui._discovery.response.explanation)) {
+        $.each(ui._discovery.response.explanation, $.proxy(function (idx, exp) {
+          if (exp.criterionValue && $.isArray(exp.criterionValue.placemarks)) {
+            $.each(exp.criterionValue.placemarks, $.proxy(function (idx, pm) {
+              $.each($.simplicityGeoJsonToBing(pm), $.proxy(function (idx, marker) {
+                if (marker.type !== 'Error') {
+                  marker.geojson = pm;
+                  var markerEvent = {
+                    map: this._map,
+                    marker: marker
+                  };
+                  this._trigger('marker', {}, markerEvent);
+                  marker = markerEvent.marker;
+                  if ('undefined' !== typeof marker) {
+                    this._markers.push(marker);
+                    this._map.entities.push(marker);
+                    console.log(marker, $.isFunction(marker.getLocations) ? marker.getLocations() : []);
+                  }
+                }
+              }, this));
+            }, this));
+          }
+        }, this));
+      }
+    },
+    destroy: function () {
+      this.element.removeClass('ui-simplicity-bing-map-criteria');
+      $(this.options.searchElement).unbind('simplicitySearchResponse', this._responseHandler);
+      this.element.unbind('simplicitybingmapboundscoordinatorcalculatebounds', this._calcBoundsHandler);
+      delete this._map;
+      $.Widget.prototype.destroy.apply(this, arguments);
+    }
+  });
+}(jQuery));
