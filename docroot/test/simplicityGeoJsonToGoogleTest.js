2ec1594bd8352293a946272995194e81d31c70f3
Eric Meyer <eric@t11e.com>
2011-04-06 17:12:49 -0400

    Adds more tests for simplicityGeoJsonToGoogleTest. Reshuffles jsTestDriver.conf.

--- docroot/test/simplicityGeoJsonToGoogleTest.js
+++ docroot/test/simplicityGeoJsonToGoogleTest.js
@@ -9,8 +9,54 @@ TestCase("simplicityGeoJsonToGoogle", {
     assertNotNull(vendorObjs);
     assertTrue($.isArray(vendorObjs));
     assertEquals('should contain converted', 1, vendorObjs.length);
-    assertTrue('should be expected type', vendorObjs[0] instanceof google.maps.Marker);
+    assertInstanceOf('should be expected type', google.maps.Marker, vendorObjs[0]);
     assertEquals('should have lat', 10.0, vendorObjs[0].getPosition().lat());
     assertEquals('should have lon', 100.0, vendorObjs[0].getPosition().lng());
+  },
+  'testFeatureCollection': function () {
+    var vendorObjs = $.simplicityGeoJsonToGoogle(
+        { 'type': 'FeatureCollection',
+          'features': [
+            { 'type': 'Feature',
+              'id': 'point1',
+              'geometry': {'type': 'Point', 'coordinates': [102.0, 0.5]},
+              'properties': {'name': 'point 1 name'}
+              },
+            { 'type': 'Feature',
+              'id': 'linestr1',
+              'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                  [102.0, 0.0], [103.0, 1.0], [103.9, 0.0], [105.0, 1.0]
+                  ]
+                },
+              'properties': {
+                'prop0': 'value0',
+                'prop1': 0.0
+                }
+              }
+             ]
+           }
+    );
+    assertNotNull(vendorObjs);
+    assertTrue($.isArray(vendorObjs));
+    assertEquals('should contain converted', 2, vendorObjs.length);
+
+    (function () {
+      var actual = vendorObjs[0][0];
+      assertInstanceOf('should be expected type', google.maps.Marker, actual);
+      assertEquals('should have lat', 0.5, actual.getPosition().lat());
+      assertEquals('should have lon', 102.0, actual.getPosition().lng());
+    }());
+    (function () {
+      var actual = vendorObjs[1][0];
+      assertInstanceOf('should be expected type', google.maps.PolyLine, actual);
+      var path = actual.getPath();
+      assertEquals('should vertices', 4, path.getLength());
+      assertEquals('should have vertex', [102.0D, 0.0D], [path.getAt(0).lng(), path.getAt(0).lat()]);
+      assertEquals('should have vertex', [103.0D, 1.0D], [path.getAt(1).lng(), path.getAt(1).lat()]);
+      assertEquals('should have vertex', [103.9D, 0.0D], [path.getAt(2).lng(), path.getAt(2).lat()]);
+      assertEquals('should have vertex', [105.0D, 1.0D], [path.getAt(3).lng(), path.getAt(3).lat()]);
+    }());
   }
 });
