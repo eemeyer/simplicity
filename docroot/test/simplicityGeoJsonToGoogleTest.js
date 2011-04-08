49b2c9139bd686ffdd22d18da591811d8e6e0f30
Eric Meyer <eric@t11e.com>
2011-04-08 15:34:17 -0400

    Updates test after review with Terry.

--- docroot/test/simplicityGeoJsonToGoogleTest.js
+++ docroot/test/simplicityGeoJsonToGoogleTest.js
@@ -10,26 +10,24 @@ TestCase("simplicityGeoJsonToGoogle", {
     assertArray(converted.vendorObjects);
     assertObject(converted.geoJson);
 
-    var vendorObjects = converted.vendorObjects;
-    assertEquals('should contain converted', 1, vendorObjects.length);
-    assertInstanceOf('should be expected type', google.maps.Marker, vendorObjects[0]);
-    assertEquals('should have lat', 10.0, vendorObjects[0].getPosition().lat());
-    assertEquals('should have lon', 100.0, vendorObjects[0].getPosition().lng());
-    assertObject(vendorObjects[0].simplicityGeoJson);
+    assertEquals('should contain converted', 1, converted.vendorObjects.length);
+    assertInstanceOf('should be expected type', google.maps.Marker, converted.vendorObjects[0]);
+    assertEquals('should have lat', 10.0, converted.vendorObjects[0].getPosition().lat());
+    assertEquals('should have lon', 100.0, converted.vendorObjects[0].getPosition().lng());
+    assertObject(converted.vendorObjects[0].simplicityGeoJson);
     assertEquals(
       {'type': 'Feature',
       'id': 'identifier',
       'geometry': {'type': 'Point', 'coordinates': [100.0, 10.0] },
-      'properties': {'name': 'named'},
-      simplicityGeoJsonParent: null
+      'properties': {'name': 'named'}
       },
-      vendorObjects[0].simplicityGeoJson);
+      converted.vendorObjects[0].simplicityGeoJson);
     assertEquals(
         {'type': 'Feature',
         'id': 'identifier',
         'geometry': {'type': 'Point', 'coordinates': [100.0, 10.0] },
         'properties': {'name': 'named'},
-        simplicityVendorObject: vendorObjects[0]
+        simplicityVendorObject: converted.vendorObjects[0]
         },
         converted.geoJson);
   },
@@ -59,19 +57,18 @@ TestCase("simplicityGeoJsonToGoogle", {
     assertObject(converted);
     assertArray(converted.vendorObjects);
     assertObject(converted.geoJson);
-    var vendorObjects = converted.vendorObjects;
 
-    assertTrue($.isArray(vendorObjects));
-    assertEquals('should contain converted', 2, vendorObjects.length);
+    assertTrue($.isArray(converted.vendorObjects));
+    assertEquals('should contain converted', 2, converted.vendorObjects.length);
 
     (function () {
-      var actual = vendorObjects[0];
+      var actual = converted.vendorObjects[0];
       assertInstanceOf('should be expected type', google.maps.Marker, actual);
       assertEquals('should have lat', 0.5, actual.getPosition().lat());
       assertEquals('should have lon', 102.0, actual.getPosition().lng());
     }());
     (function () {
-      var actual = vendorObjects[1];
+      var actual = converted.vendorObjects[1];
       var path = actual.getPath();
       assertEquals('should vertices', 4, path.getLength());
       assertEquals('should have vertex', [102.0, 0.0], [path.getAt(0).lng(), path.getAt(0).lat()]);
@@ -87,7 +84,7 @@ TestCase("simplicityGeoJsonToGoogle", {
         'id': 'point1',
         'geometry': {'type': 'Point', 'coordinates': [102.0, 0.5]},
         'properties': {'name': 'point 1 name'},
-        simplicityVendorObject: vendorObjects[0]
+        simplicityVendorObject: converted.vendorObjects[0]
       },
       {
         'type': 'Feature',
@@ -100,36 +97,15 @@ TestCase("simplicityGeoJsonToGoogle", {
           'prop0': 'value0',
           'prop1': 0.0
         },
-        simplicityVendorObject: vendorObjects[1]
+        simplicityVendorObject: converted.vendorObjects[1]
       }]
-    }, converted);
+    }, converted.geoJson);
 
     assertEquals({
       'type': 'Feature',
       'id': 'point1',
       'geometry': {'type': 'Point', 'coordinates': [102.0, 0.5]},
-      'properties': {'name': 'point 1 name'},
-      simplicityGeoJsonParent: {
-        'type': 'FeatureCollection',
-        'features': [{
-          'type': 'Feature',
-          'id': 'point1',
-          'geometry': {'type': 'Point', 'coordinates': [102.0, 0.5]},
-          'properties': {'name': 'point 1 name'}
-        },
-        {
-          'type': 'Feature',
-          'id': 'linestr1',
-          'geometry': {
-            'type': 'LineString',
-            'coordinates': [[102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]]
-          },
-          'properties': {
-            'prop0': 'value0',
-            'prop1': 0.0
-          }
-        }]
-      }
-    }, vendorObjects[0].simplicityGeoJson);
+      'properties': {'name': 'point 1 name'}
+    }, converted.vendorObjects[0].simplicityGeoJson);
   }
 });
